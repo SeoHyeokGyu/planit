df37e36200ecdf4c501ba2d4d7da8b4a1b1066d5
seohyeokgyu <shk64099@gmail.com>
2025-10-14 21:49:39 +0900

    fix: OAuth2 자동 설정 비활성화 및 Gradle wrapper 업데이트
    - OAuth2ClientAutoConfiguration 제외 설정 추가 (9주차 구현 예정)
    - Gradle wrapper 파일 업데이트 (8.5 버전)
    - 애플리케이션 정상 실행 확인

--- src/main/kotlin/com/planit/PlanitApplication.kt
+++ src/main/kotlin/com/planit/PlanitApplication.kt
@@ -1,9 +1,10 @@
 package com.planit
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [OAuth2ClientAutoConfiguration::class])
 class PlanitApplication
 
 fun main(args: Array<String>) {
