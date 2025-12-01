d50e978797538d18b7ae08d92bd545623179b94e
seohyeokgyu <shk64099@gmail.com>
2025-12-01 21:44:16 +0900

    feat: 피드 타입 Enum 추가
    실시간 피드 기능을 위한 FeedType enum 생성
    
    피드 타입:
    - CERTIFICATION: 챌린지 인증
    - ACHIEVEMENT: 배지 획득
    - LEVEL_UP: 레벨 업
    - STREAK_MILESTONE: 스트릭 달성
    - CHALLENGE_START: 챌린지 시작
    
    Phase 1: 백엔드 기반 구축 (1/4)

---
+++ src/main/kotlin/com/planit/enums/FeedType.kt
@@ -0,0 +1,21 @@
+package com.planit.enums
+
+/**
+ * 피드 타입
+ */
+enum class FeedType {
+    /** 챌린지 인증 */
+    CERTIFICATION,
+
+    /** 배지 획득 */
+    ACHIEVEMENT,
+
+    /** 레벨 업 */
+    LEVEL_UP,
+
+    /** 스트릭 달성 */
+    STREAK_MILESTONE,
+
+    /** 챌린지 시작 */
+    CHALLENGE_START
+}
